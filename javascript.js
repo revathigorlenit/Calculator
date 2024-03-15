1dc00926ad6cadaeaace09e54b6df805c1ae268c
revathigorlenit <163101644+revathigorlenit@users.noreply.github.com>
2024-03-15 12:46:09 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let display = document.getElementById('display');
+
+function Display(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+function Result() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
